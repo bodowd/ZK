89cd93a59775164b91d574dd4022e720cb64f360
Bing O'Dowd <Bing@wirelessprv-10-193-195-85.near.illinois.edu>
2017-06-17 09:08:54 -0500

    initial commit

---
+++ ZKTools.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.preprocessing import Imputer
+from sklearn.ensemble import RandomForestRegressor
+
+class CV:
+    """
+    Cross validation class
+
+    """
+    def __init__(self, df_train, df_target, n_splits, model, **params):
+        self.params = params
+        self.df_train = df_train
+        self.df_target = df_target
+        self.n_splits = n_splits
+        self.model = model
+    
+
+    def cross_validate(self, scoring = 'neg_mean_absolute_error'):
+        kf = KFold(n_splits = self.n_splits, random_state = 0)
+        self.model.fit(self.df_train, self.df_target)
+        results = cross_val_score(self.model, self.df_train, self.df_target, cv = kf, scoring = scoring)
+        return results.mean(), results.std()
+   
+    def plot_importances(self, filename = None):
+        features = self.model.feature_importances_
+        cols = self.df_train.columns
+        feature_dataframe = pd.DataFrame({'features': cols,
+        'Random Forest feature importances': features})
+        # # Feature importances bar plot
+        ax = feature_dataframe.plot(kind = 'barh', legend = None)
+        ax.set_yticklabels(feature_dataframe['features'].values)
+        plt.tight_layout()
+        plt.savefig('{}Plots/{}.png'.format(path, filename))
+
+    # def cv_paramters(parameter = []):
+        # for param in parameter: 
+            # cross_validate(
+
+
+# -------------------------------------------------------
+# tests
+print('Loading data...')
+path = '/Users/Bing/Documents/DS/Zillow_Kaggle/'
+df_train = pd.read_csv('train_features.csv')
+df_target = pd.read_csv('train_target.csv').values.ravel()
+imp = Imputer()
+df_train_imp = pd.DataFrame(imp.fit_transform(df_train), columns = df_train.columns)
+
+rf_params = {
+    'n_jobs': -1,
+    'n_estimators': 10,
+    'warm_start': True,
+    'max_depth' : 6,
+    'min_samples_leaf': 2,
+    'max_features' : 'sqrt',
+    'verbose': 0
+    }
+rf = RandomForestRegressor(random_state = 0, **rf_params) 
+
+rf_CV = CV(df_train = df_train_imp, df_target = df_target, n_splits = 10, model = rf, params = rf_params) 
+mae_mean, mae_std = rf_CV.cross_validate()
+print('RF MAE: {} ({})'.format(mae_mean, mae_std))
+rf_CV.plot_importances('rf_6_17_test')
